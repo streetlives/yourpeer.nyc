73d5532e0ef6798d7613dfb53ef8ff66a42e294f
Jacob Beard <jake@minnow.io>
2024-11-24 17:08:02 -0700

    Check in missing login module

---
+++ src/app/[route]/@staticPage/login.tsx
@@ -0,0 +1,30 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '@/amplify/amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+import { signIn, SignInInput, SignInOutput } from 'aws-amplify/auth';
+import { useRouter } from 'next/navigation';
+
+Amplify.configure(outputs);
+
+
+export function LoginPage() {
+  const router = useRouter();
+  return (
+    <div style={{ paddingTop: "5em", paddingBottom: "1em" }}>
+      <Authenticator 
+        services={{
+            handleSignIn: async (input: SignInInput): Promise<SignInOutput> => {
+                const result = await signIn(input);
+                if(result.isSignedIn){
+                    // TODO: redirect to wherever we were before we clicked the login button
+                    router.push("/");
+                }
+                return result;
+            }
+        }}
+        hideSignUp 
+        />
+    </div>
+  );
+}
