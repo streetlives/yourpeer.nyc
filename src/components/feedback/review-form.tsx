ea84a5c504890feca8339b8f067a2fe4dc688dfe
shakil hossain <sh9743441@gmail.com>
2025-03-31 15:08:23 +0600

    format

--- src/components/feedback/review-form.tsx
+++ src/components/feedback/review-form.tsx
@@ -77,7 +77,7 @@ export default function ReviewForm({
   const containsURL = (value: string) => {
     const urlRegex = new RegExp(
       "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/\\.\\w-_]*)?\\??(?:[-\\+=&;%@\\.\\w_]*)#?(?:[\\.\\!\\/\\\\\\w]*))?)",
-      "i"
+      "i",
     );
     return !urlRegex.test(value) || "Text should not contain URLs";
   };
