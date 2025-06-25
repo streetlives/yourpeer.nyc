46a774676a943712dc4c12a90e5113a846df3a22
Jacob Beard <jake@minnow.io>
2025-06-24 22:01:20 -0600

    Add logging

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "default-no-store";
 
 export default function robots(): MetadataRoute.Robots {
+  console.log('"IS_PROD" in process.env', "IS_PROD" in process.env);
   return "IS_PROD" in process.env
     ? {
         rules: [
