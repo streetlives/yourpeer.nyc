58c118856032a4a6f39fd82d7bf01b6700512a4b
shakil hossain <sh9743441@gmail.com>
2025-07-07 17:58:34 +0600

    custom streetview

--- src/components/custom-streetviews.ts
+++ src/components/custom-streetviews.ts
@@ -103,6 +103,12 @@ const customStreetViews: Record<string, string> = {
   "safe-horizon-fort-greene": "40.6874692,-73.9798123&fov=75&heading=51",
   "morris-heights-health-center-mhhc-melrose":
     "40.8221619,-73.9149579&fov=75&heading=296",
+  "council-of-peoples-organization-copo-ditmas-park":
+    "40.6313213,-73.96646&fov=39&heading=87&pano=OB3HC6m_bky8xISw4CxZNA",
+  "bay-ridge-center-bay-ridge-15-bay-ridge-ave":
+    "40.6385644,-74.0351451&fov=75&heading=90",
+  "strive-aka-east-harlem-employment-services-east-harlem":
+    "40.8016565,-73.9369507&fov=75&heading=50&pano=4cAi0Lm5z7LW57x6x0KU4Q",
 };
 
 export default customStreetViews;
