af8f657b98496888abb44d9aa9e2f4d8776f483d
shakil hossain <sh9743441@gmail.com>
2025-10-06 08:55:08 +0600

    #488 sticky reply button

---
+++ src/context/ScrollContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useRef } from "react";
+
+type ScrollContextType = React.RefObject<HTMLUListElement> | null;
+
+const ScrollContext = createContext<ScrollContextType>(null);
+
+export const ScrollProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const containerRef = useRef<HTMLUListElement | null>(null);
+
+  return (
+    <ScrollContext.Provider value={containerRef}>
+      {children}
+    </ScrollContext.Provider>
+  );
+};
+
+// Custom hook for consuming the scroll container ref
+export const useScrollContainer =
+  (): React.RefObject<HTMLUListElement> | null => useContext(ScrollContext);
