516ddb3f9bd163292dab4a5b34eb1d544f8150c8
Jacob Beard <jake@minnow.io>
2025-10-15 22:13:42 -0600

    Make observer a singleton

--- src/components/gtranslate-wrapper.tsx
+++ src/components/gtranslate-wrapper.tsx
@@ -33,6 +33,12 @@ function setNoTranslateOnTitle(): void {
   }
 }
 
+// Set up a Mutation Observer to watch for changes in the head
+const observer = new MutationObserver(() => {
+  setNoTranslateOnTitle();
+});
+
+
 export default function GTranslateWrapper() {
   const GOOGLE_TRANSLATE_ELEMENT_ID = "google_translate_element";
   const SCRIPT_SRC =
@@ -52,11 +58,6 @@ export default function GTranslateWrapper() {
     function poll() {
       // do not translate the title, in order to prevent Google Translate from breaking the page
       setNoTranslateOnTitle();
-
-      // Set up a Mutation Observer to watch for changes in the head
-      const observer = new MutationObserver(() => {
-        setNoTranslateOnTitle();
-      });
 
       // Observe the <head> element for child list changes (like a new <title>)
       const headElement = document.querySelector('head');
