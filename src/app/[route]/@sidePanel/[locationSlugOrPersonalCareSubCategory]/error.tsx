b565793075947f30aa561b135accf26964b0dc87
shakil hossain <sh9743441@gmail.com>
2025-10-17 16:54:17 +0600

    create re-usable error component

--- src/app/[route]/@sidePanel/[locationSlugOrPersonalCareSubCategory]/error.tsx
+++ src/app/[route]/@sidePanel/[locationSlugOrPersonalCareSubCategory]/error.tsx
@@ -1,6 +1,6 @@
 "use client"; // Error boundaries must be Client Components
 
-import { Button } from "@/components/ui/button";
+import ErrorComponent from "@/components/error";
 import { useEffect } from "react";
 
 export default function Error({
@@ -14,21 +14,5 @@ export default function Error({
     console.error(error);
   }, [error]);
 
-  return (
-    <section className="bg-white flex-1 py-12 lg:py-20">
-      <div className="px-5 max-w-lg mx-auto">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-black text-2xl md:text-4xl font-medium mb-6 text-center">
-            Oops!
-          </h1>
-          <p className="text-dark text-center mb-6">
-            Something went wrong. Please try again.
-          </p>
-          <div className="flex flex-col justify-center items-center space-y-2">
-            <Button onClick={() => reset()}>Try again</Button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  return <ErrorComponent onClick={() => reset()} />;
 }
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
-
-// Error boundaries must be Client Components
+import ErrorComponent from "@/components/error";
 
 export default function GlobalError({
   error,
@@ -14,21 +12,7 @@ export default function GlobalError({
     // global-error must include html and body tags
     <html>
       <body>
-        <section className="bg-white flex-1 py-12 lg:py-20">
-          <div className="px-5 max-w-lg mx-auto">
-            <div className="flex flex-col items-center justify-center">
-              <h1 className="text-black text-2xl md:text-4xl font-medium mb-6 text-center">
-                Oops!
-              </h1>
-              <p className="text-dark text-center mb-6">
-                Something went wrong. Please try again.
-              </p>
-              <div className="flex flex-col justify-center items-center space-y-2">
-                <Button onClick={() => reset()}>Try again</Button>
-              </div>
-            </div>
-          </div>
-        </section>
+        <ErrorComponent onClick={() => reset()} />
       </body>
     </html>
   );
