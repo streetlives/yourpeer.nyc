b4c8c941f8161f5b9bd979a31d291b61fc0ab1a3
shakil hossain <sh9743441@gmail.com>
2025-10-17 16:47:59 +0600

    add custom error pages

---
+++ src/app/[route]/@sidePanel/[locationSlugOrPersonalCareSubCategory]/error.tsx
@@ -0,0 +1,34 @@
+"use client"; // Error boundaries must be Client Components
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <section className="bg-white flex-1 py-12 lg:py-20">
+      <div className="px-5 max-w-lg mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-black text-2xl md:text-4xl font-medium mb-6 text-center">
+            Oops!
+          </h1>
+          <p className="text-dark text-center mb-6">
+            Something went wrong. Please try again.
+          </p>
+          <div className="flex flex-col justify-center items-center space-y-2">
+            <Button onClick={() => reset()}>Try again</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
