e9f7905286cfb041aa49487d1416d45d4a5ce891
shakil hossain <sh9743441@gmail.com>
2025-10-24 18:29:31 +0600

    remove comments

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,5 @@
 import { create } from "zustand";
 
-// interface LocationsQuery {
-//   category?: Category | null;
-//   subcategory?: SubCategory | null;
-//   openingHours?: "open" | "any";
-//   requirementsType?: ParsedRequirements;
-// }
-
 interface FiltersStore {
   isOpen: boolean;
   resultsCount?: number;
@@ -15,11 +8,6 @@ interface FiltersStore {
   close: () => void;
   isLoading: boolean;
   setLoading: (value: boolean) => void;
-  // query: LocationsQuery;
-  // setCategory: (category: Category) => void;
-  // setSubcategory: (subcategory: SubCategory | null) => void;
-  // setOpeningHours: (openingHours: "open" | "any") => void;
-  // setRequirementsType: (req: ParsedRequirements) => void;
 }
 
 export const useFilters = create<FiltersStore>((set) => ({
@@ -30,12 +18,4 @@ export const useFilters = create<FiltersStore>((set) => ({
   close: () => set({ isOpen: false }),
   updateResultCount: (count: number) => set({ resultsCount: count }),
   setLoading: (value) => set({ isLoading: value }),
-  // query: {},
-  // setCategory: (category) => set((store) => ({ query: { category } })),
-  // setSubcategory: (subcategory) =>
-  //   set((store) => ({ query: { ...store.query, subcategory } })),
-  // setRequirementsType: (requirementsType) =>
-  //   set((store) => ({ query: { ...store.query, requirementsType } })),
-  // setOpeningHours: (openingHours) =>
-  //   set((store) => ({ query: { ...store.query, openingHours } })),
 }));
