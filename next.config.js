00c5826710d4eec0401618bc48e9f92fb6c8b553
Jacob Beard <jake@minnow.io>
2025-12-12 15:41:25 -0700

    Enable real source maps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-  //experimental: {
-  //  instrumentationHook: true,
-  //},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.devtool = "eval-source-map";
+    }
+    return config;
+  },
   pageExtensions: ["ts", "tsx"],
   transpilePackages: ["@tanstack/react-query", "@tanstack/query-core"],
+
+  // Optional: helps client-side traces too
+  productionBrowserSourceMaps: true,
 };
+
+module.exports = nextConfig;
