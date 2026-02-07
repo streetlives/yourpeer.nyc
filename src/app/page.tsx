d06fbabd8b45a774e0af5a9a02611812a0adc1a6
shakil hossain <sh9743441@gmail.com>
2026-02-07 18:35:42 +0600

    add fetchPriority

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,7 @@ export default async function HomePage() {
           alt="Hero background"
           fill
           priority
+          fetchPriority="high"
           quality={60}
           sizes="100vw"
           className="object-cover"
