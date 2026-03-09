b65268eeeb8f536f6182c3afe9811682c33f0fc5
shakil hossain <sh9743441@gmail.com>
2026-03-09 14:51:01 +0600

    update link to a tag

--- src/components/ExploreServicesButton.tsx
+++ src/components/ExploreServicesButton.tsx
@@ -1,5 +1,4 @@
 import { TranslatableText } from "@/components/translatable-text";
-import Link from "next/link";
 
 type ExploreServicesButtonProps = {
   className?: string;
@@ -9,8 +8,8 @@ export default function ExploreServicesButton({
   className = "primary-button",
 }: ExploreServicesButtonProps) {
   return (
-    <Link className={className} href={"/locations"}>
+    <a className={className} href={"/locations"}>
       <TranslatableText text="Explore services" />
-    </Link>
+    </a>
   );
 }
