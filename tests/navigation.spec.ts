a35f756e9b56a760b0623c9cb4b3f92cc8f6bb51
shakil hossain <sh9743441@gmail.com>
2026-03-14 11:27:16 +0600

    assert heading visivility in locations page

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -5,9 +5,20 @@ test("should navigate to the locations page after clicking 'Explore Services'", 
 }) => {
   await page.goto("/");
 
-  // Click the "Explore Services" link
   await page.getByRole("link", { name: "Explore Services" }).first().click();
 
-  // Verify that the URL is correct
-  await test.expect(page).toHaveURL(/\/locations$/);
+  await test.expect(page).toHaveURL("/locations");
+  await test
+    .expect(page.getByRole("heading", { name: "All service locations" }))
+    .toBeVisible();
+});
+
+test("should navigate to home page after clicking the logo", async ({
+  page,
+}) => {
+  await page.goto("/locations");
+
+  await page.getByRole("link", { name: "YourPeerNYC" }).click();
+
+  await test.expect(page).toHaveURL("/");
 });
