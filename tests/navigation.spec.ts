2bed92be2780c2e1795c649b44984facc0b3e5ec
shakil hossain <sh9743441@gmail.com>
2026-03-14 10:32:56 +0600

    run web server

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -3,11 +3,11 @@ import test from "@playwright/test";
 test("should navigate to the locations page after clicking 'Explore Services'", async ({
   page,
 }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   // Click the "Explore Services" link
   await page.getByRole("link", { name: "Explore Services" }).first().click();
 
   // Verify that the URL is correct
-  await test.expect(page).toHaveURL("http://localhost:3000/locations");
+  await test.expect(page).toHaveURL(/\/locations$/);
 });
