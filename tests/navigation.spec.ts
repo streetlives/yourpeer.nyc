0087dde9e91a28ac9413fb8610e729cceb456cf2
Shakil Hossain <82315234+shakilhossain1@users.noreply.github.com>
2026-03-16 09:14:54 +0600

    Setup Playwright test (#628)
    * Setup Playwright test
    
    * run web server
    
    * assert heading visivility in locations page
    
    * updat test
    
    * add env in github actions.
    
    * add google map api key

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,24 @@
+import test from "@playwright/test";
+
+test("should navigate to the locations page after clicking 'Explore Services'", async ({
+  page,
+}) => {
+  await page.goto("/");
+
+  await page.getByRole("link", { name: "Explore Services" }).first().click();
+
+  await test.expect(page).toHaveURL("/locations");
+  await test
+    .expect(page.getByRole("heading", { name: "All service locations" }))
+    .toBeVisible();
+});
+
+test("should navigate to home page after clicking the logo", async ({
+  page,
+}) => {
+  await page.goto("/locations");
+
+  await page.getByRole("link", { name: "YourPeerNYC" }).click();
+
+  await test.expect(page).toHaveURL("/");
+});
