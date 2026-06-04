f3598248a5a0e919da905f7c200c14059be88e5c
shakil hossain <sh9743441@gmail.com>
2026-06-04 08:40:18 +0600

    test: update Playwright installation to specify Chromium and adjust project configuration for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,21 +33,29 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+        {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        },
+      ],
 
     /* Test against mobile viewports. */
     // {
